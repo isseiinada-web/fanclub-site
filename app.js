dea71fd5af564f0d6c8e8e084167e5c0363febbe
isseiinada-web <isseiinada@gmail.com>
2026-05-20 21:31:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ import {
   query,
   orderBy,
   onSnapshot,
-  serverTimestamp
+  serverTimestamp,
   deleteDoc,
-　getDocs
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
